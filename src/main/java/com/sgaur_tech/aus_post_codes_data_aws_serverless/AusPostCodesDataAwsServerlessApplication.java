b80bab1a7146d8ea539ccad2d38dc3fc1821a319
shivamacshack <s.gaur@uq.edu.au>
2026-03-31 21:58:40 +1100

    Initial commit

---
+++ src/main/java/com/sgaur_tech/aus_post_codes_data_aws_serverless/AusPostCodesDataAwsServerlessApplication.java
@@ -0,0 +1,13 @@
+package com.sgaur_tech.aus_post_codes_data_aws_serverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AusPostCodesDataAwsServerlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AusPostCodesDataAwsServerlessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgaur_tech/aus_post_codes_data_aws_serverless/AusPostCodesDataAwsServerlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgaur_tech.aus_post_codes_data_aws_serverless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AusPostCodesDataAwsServerlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
